f37a9f2c4a85f84c2aa6d9e7f802bc270b20db01
Karl Matthias <karl@matthias.org>
2022-02-19 20:51:34 +0000

    Refactor, clean up, still works

--- hello.asm
+++ hello.asm
@@ -4,35 +4,38 @@
 
 BDOS  equ 5
 
-	org		0100h
+  org 0100h
 
-	jr start
+  jr start
 
+msg: BYTE "\r\nHello, World!\r\n$"
+
+; BDOS call to print a character to console
 ; e must contain the char to print
 print_char:
-	ld 		c, 02h
-	call 	BDOS
-	ret
+  ld    c, 2 
+  call  BDOS
+  ret
 
+; BDOS call to print a string to console
 ; de must contain address of string, terminated with $
 print_string:
-	ld		c, 9    	; CP/M write string to console call
-	call	BDOS
-	ret
-	
+  ld    c, 9          ; CP/M write string to console call
+  call  BDOS
+  ret
+  
+; BDOS call to exit and return to CP
 ; No args
 reset:
-	ld		c, 0    	; CP/M system reset call - shut down
-	call	BDOS
-	
-
-msg: BYTE "\r\nHello, World!$"
-
+  ld    c, 0          ; CP/M system reset call - shut down
+  call  BDOS
+  
 start:
-	ld 		e,'k'
-	call	print_char
-	ld   	de, msg
-	call 	print_string
+  ld    e,'k'
+  call  print_char    ; Print the letter 'k'
 
-	call	reset	
-	halt						; This code is never reached
+  ld    de, msg
+  call  print_string  ; Print the msg string
+
+  call  reset  
+  halt                ; This code is never reached
